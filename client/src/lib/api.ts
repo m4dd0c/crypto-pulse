0b23f3a6567e28af9f4e52430d36bc7889dc9959
Manish Suthar <manishsuthar078@gmail.com>
2025-07-02 16:36:43 +0530

    feat: api.ts added, containing routes and server_url

---
+++ client/src/lib/api.ts
@@ -0,0 +1,3 @@
+export const SERVER_URL = "http://localhost:4000";
+export const downloadReportURL = `${SERVER_URL}/api/v1/download-report`;
+export const priceHistoryURL = `${SERVER_URL}/api/v1/price-history`;
